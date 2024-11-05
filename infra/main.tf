aa41c1fafa47e582689cdf607754f45883446625
Milena Alcantara <milena.alcantara@ZWBC13XG3.servidormultilidae@gmail.com>
2024-11-05 13:49:01 -0300

    First Commit

---
+++ infra/main.tf
@@ -0,0 +1,24 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.16"
+    }
+  }
+
+  required_version = ">= 1.2.0"
+}
+
+provider "aws" {
+  region  = var.aws_region
+}
+
+resource "aws_instance" "ec2-example" {
+  ami           = var.instance_ami_id
+  instance_type = var.instance_type
+
+  tags = {
+    Name        = var.instance_name
+    description = var.instance_description
+  }
+}
---
+++ infra/variables.tf
@@ -0,0 +1,29 @@
+variable "aws_region" {
+  description = "The region AWS"
+  type        = string
+  default     = "sa-east-1"
+}
+
+variable "instance_name" {
+  description = "The name of the EC2 instance"
+  type        = string
+  default     = "ec2-example" 
+}
+
+variable "instance_description" {
+  description = "The description tag of the EC2 instance"
+  type        = string
+  default     = "I'm in the clouds" 
+}
+
+variable "instance_type" {
+  description = "The type of the EC2 instance"
+  type        = string
+  default     = "t3.micro"  
+}
+
+variable "instance_ami_id" {
+  description = "The AMI id of the EC2 instance"
+  type        = string
+  default     = "ami-0989c1b438266c944"  
+}
