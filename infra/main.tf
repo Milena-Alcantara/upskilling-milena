87ea2a7954608b1063a46cecbc789182ce87001b
Milena Alcantara <milena.alcantara@ZWBC13XG3.servidormultilidae@gmail.com>
2024-11-05 15:06:39 -0300

    Add correcoes

--- infra/main.tf
+++ infra/main.tf
@@ -2,11 +2,11 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "~> 4.16"
+      version = "5.74.0"
     }
   }
 
-  required_version = ">= 1.2.0"
+  required_version = ">= 1.5.5"
 }
 
 provider "aws" {
